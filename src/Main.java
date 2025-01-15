b2772582faf15a518a896c7aa3d25e228f65bb1b
paiva <saviopaivajr@gmail.com>
2025-01-15 20:56:03 -0300

    classes curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setCargaHoraria(8);
+        curso1.setDescricao("sei la");
+        curso1.setTitulo("Matematica");
+
+        Mentoria ment = new Mentoria();
+        ment.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(ment);
+
     }
 }
