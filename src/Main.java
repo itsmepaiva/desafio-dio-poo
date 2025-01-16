01a7691554b5f44babdb525d2ec8fcc5ffc0046c
paiva <saviopaivajr@gmail.com>
2025-01-15 21:08:06 -0300

    classes curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,17 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setCargaHoraria(8);
         curso1.setDescricao("sei la");
-        curso1.setTitulo("Matematica");
+        curso1.setTitulo("curso java");
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("sei nao");
+        curso2.setCargaHoraria(4);
 
         Mentoria ment = new Mentoria();
         ment.setData(LocalDate.now());
+        ment.setDescricao("dunno");
+        ment.setTitulo("mentoria java");
 
         System.out.println(curso1);
         System.out.println(ment);
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,17 +2,13 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     private LocalDate data;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
     }
 
     public LocalDate getData() {
@@ -23,19 +19,12 @@ public class Mentoria {
         this.data = data;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
